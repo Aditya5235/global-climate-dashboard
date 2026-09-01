3cac32dff3d4925e6f8c0c02437989d9f9245219
Aditya5235 <adityakuma92051@gmail.com>
2026-09-01 10:15:07 +0530

    co2 emission EDA complete

--- src/api/CO2_emission.py
+++ src/api/CO2_emission.py
@@ -1,26 +1,60 @@
 import requests 
 import pandas as pd
 
-def co2_emission(country_code , indicator ,start , end):
+def co2_emission():
 
 # | Indicator Code           | Description                            | Unit |
 # |
 # | ``EN.GHG.CO2.MT.CE.AR5`` | Total CO₂ emissions (excluding LULUCF) | Mt CO₂e |
 # | ``EN.GHG.CO2.PC.CE.AR5`` | CO₂ emissions per capita (excluding LULUCF) | t CO₂e per person |
 
-    url = (f'https://api.worldbank.org/v2/country/{country_code}/indicator/{indicator}?format=json&per_page=500')
-    meta_url = (f'https://api.worldbank.org/v2/indicator/{indicator}?format=json')
-    response = requests.get(url)
+    # fatching Total Co2 emission
+    url = (f"https://api.worldbank.org/v2/country/all/indicator/EN.GHG.CO2.MT.CE.AR5?format=json&per_page=20000")
+    meta_url = (f'https://api.worldbank.org/v2/indicator/EN.GHG.CO2.MT.CE.AR5?format=json')
+    response = requests.get(url , timeout = 20)
     meta_response = requests.get(meta_url)
 
     raw = response.json()
     metadata = meta_response.json()[1][0]
     data = raw[1] 
 
-    df = pd.DataFrame(data)
-    df = df[['date' , 'value' , 'countryiso3code']]
-    df = df.rename(columns = {'countryiso3code' : 'Country_code'})
-    df['date'] = df['date'].astype(int)
-    df['value'] = df['value'].astype(float)
+# extracting usefull data for Total Co2 emission
 
-    return df , metadata
+    total_em = pd.DataFrame(data)
+
+    total_em = total_em[['date' , 'value' , 'countryiso3code']]
+    
+    total_em = total_em.rename(columns = {'countryiso3code' : 'Country_code'})
+    total_em = total_em.rename(columns = {'value' : 'T_CO₂_emi(in mt)'})
+
+    total_em['date'] = total_em['date'].astype(int)
+    total_em['T_CO₂_emi(in mt)'] = total_em['T_CO₂_emi(in mt)'].astype(float)
+
+    # fatching Per_capita Co2 emission
+
+    url = (f"https://api.worldbank.org/v2/country/all/indicator/EN.GHG.CO2.PC.CE.AR5?format=json&per_page=20000")
+    response = requests.get(url ,timeout = 20)    
+
+    raw = response.json() 
+    data = raw[1] 
+
+    per_capita_em = pd.DataFrame(data)
+
+    per_capita_em = per_capita_em[ ['date' , 'value' , 'countryiso3code'] ] 
+
+    per_capita_em['value'] = per_capita_em['value'].astype(float)
+    per_capita_em['date'] = per_capita_em['date'].astype(int)
+
+    per_capita_em = per_capita_em.rename(columns ={'value':'Co2_per_capita(in ton)'})
+    per_capita_em = per_capita_em.rename(columns = {'countryiso3code' : 'Country_code'})
+    
+    # concating percapita data with total co2 emission
+
+    df = pd.merge(total_em , per_capita_em ,  on =['Country_code', 'date'])
+   
+
+   
+
+    return df , metadata  
+
+
